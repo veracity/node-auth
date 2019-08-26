728f7529cbe91e21d1af4a03a0e76ee1c64ed44c
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2019-08-26 08:35:07 +0200

    Improved deprecation support

--- README.md
+++ README.md
@@ -448,7 +448,7 @@ The library makes use of several data structures. They are all defined as TypeSc
 
 Property|Type|Description
 -|-|-
-secret|string|A unique string that is used to sign the session id.<br>This MUST NOT be shared with any other application.
+secret⬇|string|**Deprecated:** Borked - A unique string that is used to sign the session id.<br>This MUST NOT be shared with any other application.
 store|Store \| MemoryStore|A store instance where session data will be stored.<br>You MUST provide this otherwise express-session will default to using the insecure memory store.
 
 ### ISetupAuthFlowOptions
--- scripts/typeParser/typesToMarkdown.ts
+++ scripts/typeParser/typesToMarkdown.ts
@@ -22,11 +22,16 @@ const propsToMarkdownTableRows = (props: IInterfaceProperty[]) => {
 	const table = `Property|Type|Description
 -|-|-
 `
+	const desc = (prop: IInterfaceProperty) =>
+	 (prop.deprecatedMessage ? "**Deprecated:** "+
+	 	escapePipe(prop.deprecatedMessage.trim().replace(/(\n|\r|\n\r)\s*/, " ")+" - ") : "") +
+		escapePipe(prop.description.replace(/(\n|\r|\n\r)\s*/, "<br>"))
+
 	return table + props.map((prop) => {
 		return [
-			`${escapePipe(propNameToString(prop))}`,
+			escapePipe(propNameToString(prop)),
 			escapePipe(prop.type.trim()),
-			escapePipe(prop.description.replace(/(\n|\r|\n\r)\s*/, "<br>"))
+			desc(prop)
 		].join("|")
 	}).join("\n")
 }
