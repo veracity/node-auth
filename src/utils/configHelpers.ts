ea0511a479ea224e87c45ab095121aff82bd1cb8
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-09-03 09:58:14 +0200

    updated depts, added errorPath config to mergeConfig function

--- src/utils/configHelpers.ts
+++ src/utils/configHelpers.ts
@@ -1,7 +1,7 @@
 import { IDefaultAuthConfig, IFullAuthConfig, ISetupWebAppAuthSettings } from "../interfaces"
 
 export const mergeConfig = (defaultConfig: IDefaultAuthConfig, endUserConfig: Omit<ISetupWebAppAuthSettings, "app">): IFullAuthConfig => {
-	const { onBeforeLogin, onLoginComplete, onLoginError, onLogout, onVerify, name, logLevel, loginPath, logoutPath, policyName } = endUserConfig
+	const { onBeforeLogin, onLoginComplete, onLoginError, onLogout, onVerify, name, logLevel, loginPath, logoutPath, policyName, errorPath } = endUserConfig
 	const config = {
 		...defaultConfig,
 		oidcConfig: {
@@ -23,6 +23,7 @@ export const mergeConfig = (defaultConfig: IDefaultAuthConfig, endUserConfig: Om
 	if (name) config.name = name
 	if (loginPath) config.loginPath = loginPath
 	if (logoutPath) config.logoutPath = logoutPath
+	if (errorPath) config.errorPath = errorPath
 	if (policyName) config.policyName = policyName
 	return config
 }
